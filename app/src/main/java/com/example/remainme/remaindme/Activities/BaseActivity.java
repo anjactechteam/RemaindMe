d5cdaa66c809a6cfde510d67a7325913e5893720
anjactechteam <anjactechnicalteam@gmail.com>
2020-08-16 11:13:50 +0530

    add new task fragment & some changes in manifest

--- app/src/main/java/com/example/remainme/remaindme/Activities/BaseActivity.java
+++ app/src/main/java/com/example/remainme/remaindme/Activities/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.example.remainme.remaindme.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,6 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.remainme.remaindme.MainActivity;
 import com.example.remainme.remaindme.R;
 import com.example.remainme.remaindme.Lisitners.RecyclerTouchListener;
 import com.example.remainme.remaindme.Models.Task;
@@ -24,11 +26,11 @@ import com.example.remainme.remaindme.Adapters.TaskAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     List<Task> taskList;
     RecyclerView recyclerView;
     TaskAdapter taskAdapter;
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,16 +61,15 @@ public class BaseActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(BaseActivity.this, MainActivity.class));
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+       /* ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState();*/
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -101,7 +102,7 @@ public class BaseActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.base, menu);
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
         return true;
     }
 
@@ -114,6 +115,11 @@ public class BaseActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.main_side_bar) {
+            if(drawer.isDrawerOpen(GravityCompat.END)){
+                drawer.closeDrawer(GravityCompat.END);
+            }else{
+                drawer.openDrawer(GravityCompat.END);
+            }
             return true;
         }
 
--- app/src/main/java/com/example/remainme/remaindme/MainActivity.java
+++ app/src/main/java/com/example/remainme/remaindme/MainActivity.java
@@ -1,67 +1,41 @@
 package com.example.remainme.remaindme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
-import com.example.remainme.remaindme.Adapters.TaskAdapter;
-import com.example.remainme.remaindme.Lisitners.RecyclerTouchListener;
-import com.example.remainme.remaindme.Models.Task;
+import com.example.remainme.remaindme.Fragments.NewTaskFragment;
 
-import java.util.ArrayList;
-import java.util.List;
+public class MainActivity extends AppCompatActivity implements NewTaskFragment.OnFragmentInteractionListener {
 
-public class MainActivity extends AppCompatActivity {
-    List<Task> taskList;
-    RecyclerView recyclerView;
-    TaskAdapter taskAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        taskList =new ArrayList<>();
-        recyclerView=(RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        taskAdapter = new TaskAdapter(this,taskList);
-        recyclerView.setAdapter(taskAdapter);
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
-            @Override
-            public void onClick(View view, int position) {
-                Task movie = taskList.get(position);
-                Toast.makeText(getApplicationContext(), movie.getSchedule() + " is fucked up!", Toast.LENGTH_SHORT).show();
-            }
 
+        ((ImageButton) findViewById(R.id.go_back)).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLongClick(View view, int position) {
-
+            public void onClick(View v) {
+                onBackPressed();
+                finish();
             }
-        }));
-        prepareData();
+        });
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fl_container, new NewTaskFragment()).commit();
     }
-public void prepareData(){
-    taskList.add(new Task(1, "My first Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(2, "My Second Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(3, "My Third Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(4, "My fourth Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(5, "My fifth Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(6, "My sixth Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(7, "My seventh Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(8, "My eigth Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(9, "My nine Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(10, "My ten Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(11, "My eleven Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskList.add(new Task(12, "My twelve Title is here","Today","done","not_done","later",R.drawable.ic_schedule_black_24dp));
-    taskAdapter.notifyDataSetChanged();
-}
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        new MenuInflater(this).inflate(R.menu.activity_main_menu, menu);
-        return true;
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
